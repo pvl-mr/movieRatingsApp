64bb5e73612f5ec693b2115b42b16c2fe39b6b7a
pvl-mr <mariyapavlova2001@mail.ru>
2023-01-18 23:09:04 +0400

    call external microservice

--- movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,20 +1,43 @@
 package com.maripavlova.moviecatalogservice.resources;
 
 import com.maripavlova.moviecatalogservice.models.CatalogItem;
+import com.maripavlova.moviecatalogservice.models.Movie;
+import com.maripavlova.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Fasrer", "desc11", 5)
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        //get all rated movies for userId from rating server
+        List<Rating> ratings = Arrays.asList(
+            new Rating("movieid1", 4),
+            new Rating("movieid2", 5),
+            new Rating("movieid3", 6)
         );
+
+        //for each rated movie of userid call movie info service to get details of movies
+        return ratings.stream()
+                        .map(rating -> {
+                            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                            return new CatalogItem(movie.getName(), "description of movie", rating.getRating());
+                        })
+                        .collect(Collectors.toList());
+
+        //put ratings and movies details together
+
     }
 }
