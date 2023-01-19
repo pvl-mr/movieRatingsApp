db936f87df27fadda5919a1ae3c16661bbb945b4
pvl-mr <mariyapavlova2001@mail.ru>
2023-01-19 11:16:47 +0400

    add webclient

--- movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,10 +3,12 @@ package com.maripavlova.moviecatalogservice.resources;
 import com.maripavlova.moviecatalogservice.models.CatalogItem;
 import com.maripavlova.moviecatalogservice.models.Movie;
 import com.maripavlova.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,10 +18,14 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClient;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
-        RestTemplate restTemplate = new RestTemplate();
 
 
         //get all rated movies for userId from rating server
@@ -32,7 +38,15 @@ public class MovieCatalogResource {
         //for each rated movie of userid call movie info service to get details of movies
         return ratings.stream()
                         .map(rating -> {
-                            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                           // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+                            Movie movie = webClient.build()
+                                    .get()
+                                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                                    .retrieve()
+                                    .bodyToMono(Movie.class)
+                                    .block();
+
                             return new CatalogItem(movie.getName(), "description of movie", rating.getRating());
                         })
                         .collect(Collectors.toList());
--- movie-info-service/src/main/java/com/maripavlova/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/maripavlova/movieinfoservice/resources/MovieResource.java
@@ -1,13 +1,16 @@
 package com.maripavlova.movieinfoservice.resources;
 
 import com.maripavlova.movieinfoservice.models.Movie;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
+
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId){
