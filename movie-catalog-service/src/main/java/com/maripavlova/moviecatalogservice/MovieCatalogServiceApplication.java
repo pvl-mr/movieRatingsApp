f0cc8ebe289dd5efc819b425c34d41640053c3ea
pvl-mr <mariyapavlova2001@mail.ru>
2023-01-25 17:41:46 +0400

    enable hystrix dashboard

--- movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.reactive.HttpComponentsClientHttpConnector;
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 //@EnableHystrix
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	//LoadBalanced =
