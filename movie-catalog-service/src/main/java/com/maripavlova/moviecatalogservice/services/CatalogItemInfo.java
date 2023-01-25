ca5749ea2c3ed3212fc6ae9995d0a19829c06215
pvl-mr <mariyapavlova2001@mail.ru>
2023-01-25 17:57:00 +0400

    use bulkhead pattern

--- movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/services/CatalogItemInfo.java
+++ movie-catalog-service/src/main/java/com/maripavlova/moviecatalogservice/services/CatalogItemInfo.java
@@ -4,6 +4,7 @@ import com.maripavlova.moviecatalogservice.models.CatalogItem;
 import com.maripavlova.moviecatalogservice.models.Movie;
 import com.maripavlova.moviecatalogservice.models.Rating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,7 +14,13 @@ public class CatalogItemInfo {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+                    threadPoolKey = "movieInfoPool",
+                    threadPoolProperties = {
+                            @HystrixProperty(name="coreSize", value="20"),
+                            @HystrixProperty(name="maxQueueSize", value="10")
+                    }
+    )
     public CatalogItem getCatalogItem(Rating rating) {
         Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         //put ratings and movies details together
